b0191afd32c800dd5b66cc3b36785137be8933d0
CristianMP04 <christian.marti.pernin@gmail.com>
2022-11-04 09:34:25 +0100

    create ListadoPacientes Component
    create ListadoPacientes Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import Header from "./components/Header"
+import Formulario from "./components/Formulario"
+import ListadoPacientes from "./components/ListadoPacientes"
 
 function App() {
 
@@ -7,6 +9,8 @@ function App() {
   return (
     <> 
       <Header /> 
+      <Formulario />
+      <ListadoPacientes />
     </>
   )
 }
