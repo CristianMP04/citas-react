496d54fdc78370d48ef23108b20d30f985e67259
CristianMP04 <christian.marti.pernin@gmail.com>
2022-11-05 17:44:50 +0100

    add Mensaje Error
    add Mensaje Error

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -7,15 +7,20 @@ const Formulario = () => {
     const [fecha, setFecha] = useState('');
     const [sintomas, setSintomas] = useState('');
 
+    const [ error, setError] = useState(false);
+
     const handleSubmit = (e) => {
         e.preventDefault();
 
         // Validación del Formulario
         if( [ nombre, propietario, email, fecha, sintomas ].includes('') ) {
             console.log('Hay al menos un campo vacío');
-        } else {
-            console.log('Todos llenos');
-        }
+
+            setError(true)
+            return;
+        } 
+
+        setError(false)
     }
 
 
@@ -31,6 +36,13 @@ const Formulario = () => {
         <form
             onSubmit={handleSubmit}
             className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+            
+            { error && 
+                <div className="bg-red-800 text-white text-center p-3 uppercase font-bold mb-3 rounded-md">
+                    <p>Todos los campos son obligatorios</p>
+                </div>
+            }
+            
             <div className="mb-5">
                 <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold">
                     Nombre Mascota
